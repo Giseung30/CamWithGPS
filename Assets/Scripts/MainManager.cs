0398e63f46ea9bb84e65d2e14053ab8c77597d63
giseung30 <giseung30@gmail.com>
2023-08-12 14:36:57 +0900

    230812_MainManager GPS 테스트문 추가

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Android;
 
 public class MainManager : MonoBehaviour
 {
+    [Header("Component")]
+    public Text gpsText;
+
     private void Start()
     {
         PermissionModule permission = PermissionModule.instance;
@@ -11,10 +16,24 @@ public class MainManager : MonoBehaviour
 
     private void onPermissionGranted(string permissionName)
     {
-        WebCamModule webCam = WebCamModule.instance;
-        GPSModule gps = GPSModule.instance;
+        switch (permissionName)
+        {
+            case Permission.Camera:
+                WebCamModule.instance.SetWebCam(true);
+                break;
+            case Permission.FineLocation:
+                GPSModule.instance.SetLocationService(true);
+                break;
 
-        webCam.SetWebCam(true);
-        gps.SetLocationService(true);
+        }
+    }
+
+    private void Update()
+    {
+        GPSModule gps = GPSModule.instance;
+        if (gps.GetGPSLocation(out LocationServiceStatus status, out float latitude, out float longitude))
+            gpsText.text = $"status : {status}, latitude : {latitude}, longitude : {longitude}";
+        else
+            gpsText.text = $"Available GPS";
     }
 }
